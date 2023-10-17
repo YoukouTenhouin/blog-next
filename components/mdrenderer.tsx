eb6111d9e98638a030870d074bd24f6aef5790c9
Youkou Tenhouin <youkou@tenhou.in>
2023-10-17 14:50:47 +0800

    style: code block change

--- components/mdrenderer.tsx
+++ components/mdrenderer.tsx
@@ -8,69 +8,66 @@ import { Inconsolata } from "next/font/google";
 const codeFont = Inconsolata({ subsets: ["latin"] });
 
 function remarkCodeHandler(_, node: any) {
-  const value = node.value ? node.value + "\n" : "";
-  const lang = node.lang ? node.lang.match(/^[^ \t]+(?=[ \t]|$)/) : null;
+    const value = node.value ? node.value + "\n" : "";
+    const lang = node.lang ? node.lang.match(/^[^ \t]+(?=[ \t]|$)/) : null;
 
-  const properties: any = { value: value };
+    const properties: any = { value: value };
 
-  if (lang) {
-    properties.lang = lang;
-  }
+    if (lang) {
+        properties.lang = lang;
+    }
 
-  // Create `<mdblockcode>`
-  // We are not actually rendering this element; this is just a placeholder which will be transfered into a React component later
-  let result: any = {
-    type: "element",
-    tagName: "mdblockcode",
-    properties,
-    children: [],
-  };
+    // Create `<mdblockcode>`
+    // We are not actually rendering this element; this is just a placeholder which will be transfered into a React component later
+    let result: any = {
+        type: "element",
+        tagName: "mdblockcode",
+        properties,
+        children: [],
+    };
 
-  if (node.meta) result.data = { meta: node.meta };
+    if (node.meta) result.data = { meta: node.meta };
 
-  return result;
+    return result;
 }
 
 const CodeBlock: FC<{ lang?: string; value: string }> = ({ lang, value }) => {
-  let html = lang ? hljs.highlight(lang, value).value : value;
-  const [folded, setFolded] = useState(false)
+    let html = lang ? hljs.highlight(lang, value).value : value;
+    const [folded, setFolded] = useState(false)
 
-  return (
-    <Window>
-      <Window.TitleBar>
-        <Window.TitleButtonGroup>
-          <Window.TitleButton onClick={() => setFolded(!folded)}>➖</Window.TitleButton>
-        </Window.TitleButtonGroup>
-        <Window.TitleText>TERMINAL</Window.TitleText>
-      </Window.TitleBar>
-      <Window.Body folded={folded}>
-        <Window.Termline>
-          {"> show ./code" + (lang ? `.${lang}` : "")}
-        </Window.Termline>
-        <pre className={styles.codeblock}>
-          <code
-            className={codeFont.className}
-            dangerouslySetInnerHTML={{ __html: html }}
-          />
-        </pre>
-      </Window.Body>
-    </Window>
-  );
+    return (
+        <Window>
+            <Window.TitleBar>
+                <Window.TitleButtonGroup>
+                    <Window.TitleButton onClick={() => setFolded(!folded)}>➖</Window.TitleButton>
+                </Window.TitleButtonGroup>
+                <Window.TitleText>CODE VIEWER</Window.TitleText>
+            </Window.TitleBar>
+            <Window.Body folded={folded}>
+                <pre className={styles.codeblock}>
+                    <code
+                        className={codeFont.className}
+                        dangerouslySetInnerHTML={{ __html: html }}
+                    />
+                </pre>
+            </Window.Body>
+        </Window>
+    );
 };
 
 const MDRenderer: FC<{ content: string }> = ({ content }) => {
-  return useRemarkSync(content, {
-    remarkToRehypeOptions: {
-      handlers: {
-        code: remarkCodeHandler,
-      },
-    },
-    rehypeReactOptions: {
-      components: {
-        mdblockcode: CodeBlock,
-      },
-    },
-  });
+    return useRemarkSync(content, {
+        remarkToRehypeOptions: {
+            handlers: {
+                code: remarkCodeHandler,
+            },
+        },
+        rehypeReactOptions: {
+            components: {
+                mdblockcode: CodeBlock,
+            },
+        },
+    });
 };
 
 export default MDRenderer;
